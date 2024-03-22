e59877d76e30c47b6c69071c25c4f3d7777c28df
Carlos-AHC <cholhuin65@soy.sena.edu.co>
2024-03-22 16:52:28 -0500

    Cambios

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-"Hola" 
+const exp = require('express');
+const app = exp();
+
+const logger = require('morgan')
+app.use(logger('dev'));
+
+app.use(exp.urlencoded({extended: false}));
+app.use(exp.json())
+
+app.listen(process.env.PORT, ( )=>{
+    console.log("servidor e linea");
+} );
