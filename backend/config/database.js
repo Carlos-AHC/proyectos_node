0e55b73d62ea6fcd23923a2bdd26e51fa0e50f44
Carlos-AHC <cholhuin65@soy.sena.edu.co>
2024-04-03 15:47:06 -0500

    contraseña mongo

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const URI = `mongodb+srv : //cholguin65:<Carlosh_1035971456>@clusteradso2557466.yq2xxof.mongodb.net/?retryWrites=true&w=majority&appName=ClusterADSO2557466`
+const URI = `mongodb+srv : //cholguin65:<Itz41rLhjyRs71yl>@clusteradso2557466.yq2xxof.mongodb.net/?retryWrites=true&w=majority&appName=ClusterADSO2557466`
 mongoose.connect(URI);
 
 module.exports = mongoose
