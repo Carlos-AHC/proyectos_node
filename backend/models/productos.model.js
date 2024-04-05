b7c290db6789a1ef3c50794967e408f892e32d73
Carlos-AHC <cholhuin65@soy.sena.edu.co>
2024-04-05 15:57:24 -0500

    Campos base de datos

--- backend/models/productos.model.js
+++ backend/models/productos.model.js
@@ -1,4 +1,40 @@
+const { model } = require("mongoose");
 const mongoose = require("../config/database");
 
 const schemaProductos = new mongoose.Schema({
-})
+    referencia: {
+        type: String,
+        required: [true, 'La referencia es obligatoria']
+    },
+    nombre: {
+        type: String,
+        required: true
+    },
+    descripcion: {
+        type: String,
+        required: true
+    },
+    precio: {
+        type: Number,
+        default: [0],
+        min: [0]
+    },
+    stock: {
+        type: Number,
+        default: [0],
+        min: [0]
+    },
+    imagen: {
+        type: String,
+        required: true
+    },
+    habilitado: {
+        type: Boolean,
+        required: true
+    },
+
+
+});
+
+const producto = mongoose.model("producto", schemaProductos);
+module.exports = producto;
